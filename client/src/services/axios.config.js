09059fc713a4512815911df501bb68b06aecd528
Y-Shivansh <sharma.shivansh1305@gmail.com>
2025-07-14 00:21:47 +0530

    added frontend structure and services/axios.config.js

---
+++ client/src/services/axios.config.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export const publicApi = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+})
+
+export const privateApi = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+})
+
+// this config is provided by axios and before every req, it is asking if you want to add anything or i send it as it is.
+privateApi.interceptors.request.use((config) => {
+    const token = localStorage.getItem('authToken');
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+},
+(error) => {
+    // This is for any error that occurs before request is sent
+    console.error("Axios Interceptor Error:", error);
+    //  Axios expects a rejected Promise in that specific .use() function.
+    return Promise.reject(error); // Stop request  and Forward the error to .catch() (while sending api request.)
+}
+)
---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        dark: "#1e293b",
+        light: "#f1f5f9",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
