646d1a44b70a0a20c392552e44ce30d40832d828
Y-Shivansh <sharma.shivansh1305@gmail.com>
2025-07-26 12:32:04 +0530

    merged both branches and added ai-agent

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,9 @@
 import { AppRoutes } from "./router/AppRoutes"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ChatbotUI from "./components/chatbot/ChatbotUi";
 function App() {
   return (
     <>
-    {/* <div className=" border w-1/3 m-auto min-h-screen flex flex-col justify-center">
-<ChatbotUI/>
-
-    </div> */}
         <AppRoutes/>
         <ToastContainer position="top-right" autoClose={1000} />
     </>
