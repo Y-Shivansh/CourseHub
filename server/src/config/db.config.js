31292cf739e0b1d8044bc55a21c39653ac4fc1a5
Y-Shivansh <sharma.shivansh1305@gmail.com>
2025-11-06 04:09:16 +0530

    A Few New Changes Just Cosmetic Changes

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectToDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB Connection: ${conn.Connection.host}`);
+        console.log(`MongoDB Connection: ${conn.Connection.EventEmitter}`);
     } catch (error) {
         console.error(`MongoDB connection error: ${error.message}`);
         process.exit(1);
